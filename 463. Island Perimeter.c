4e7259df81db085c00a514046397a5b9c9836801
garrulous abyss <2945138243@qq.com>
2017-03-07 22:39:47 +0800

    Update and rename [To do]463. Island Perimeter.c to 463. Island Perimeter.c

--- 463. Island Perimeter.c
+++ 463. Island Perimeter.c
@@ -12,7 +12,7 @@ int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
         {
             if(grid[i][j] ==1)
             {
-                result = result + grid[i][j]*4;       
+                result+=4;       
                 if(j!=0 && (grid[i][j-1]==1))
                     result -=1;
                 if(j!=(gridColSize-1) && (grid[i][j+1]==1))   
@@ -27,3 +27,27 @@ int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
         return result;
     
 }
+
+//C跑出来结果总是15.。。。困惑中
+/*Java成功了的
+public class Solution {
+    public int islandPerimeter(int[][] grid) {
+      int m=grid.length;  
+      int n=grid[0].length;  
+      int result=0;  
+        for(int i=0;i<m;i++){  
+            for(int j=0;j<n;j++){  
+                if(grid[i][j]==1){  
+                    result+=4;  
+                    if((i-1)>=0&&grid[i-1][j]==1){  
+                        result-=2;  
+                    }  
+                    if((j+1)<n&&grid[i][j+1]==1)  
+                        result-=2;  
+                }  
+            }  
+        }  
+        return result;     
+    }
+}
+*/
