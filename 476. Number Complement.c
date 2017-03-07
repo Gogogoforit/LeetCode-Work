24562be57fe5e9f3443a8f22503af39c6a04b0c3
garrulous abyss <2945138243@qq.com>
2017-03-07 18:30:26 +0800

    Update 476. Number Complement.c

--- 476. Number Complement.c
+++ 476. Number Complement.c
@@ -5,14 +5,7 @@ int findComplement(int num) {
     while(temp)
     {
         temp = temp>>1;  //101,num=2.5=2=010,size=1;num=2,nume==001,size=2; num =1,num=0,size=3;
-        size++;
-    }
-    
-    while(size)
-    {
-        
         y =y<<1;         // y=2=0010,size=2;y=0100=4,size=1;y=1000,size=0;        
-        size--;
     }
     
     y = y-1;
