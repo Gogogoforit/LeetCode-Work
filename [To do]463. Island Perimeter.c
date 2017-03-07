2cc669b25db8afa14cc154e05ff709f7466f2259
garrulous abyss <2945138243@qq.com>
2017-03-07 21:43:04 +0800

    答题思路没问题，但结果不对，一会儿debug下

--- [To do]463. Island Perimeter.c
+++ [To do]463. Island Perimeter.c
@@ -1,4 +1,29 @@
 //https://leetcode.com/problems/island-perimeter/?tab=Description
 //网吧时间可能不够，先写个思路：每一个island块所自带的边长属性，被上下左右四个块的情况所约束。
 //因此最直接的思路，当然是对每个方块判断是否island，若是则判断是哪类，并把它的边长加到总的上面。
-//为了避免显然两个for循环似乎不可避（求大大指点有办法降低复杂度），然后就总的减也可以的
+
+int islandPerimeter(int** grid, int gridRowSize, int gridColSize) {
+    int i,j,result;
+    result=0;
+
+    for(i=0;i<gridRowSize;i++)
+    {
+        for(j=0;j<gridColSize;j++)
+        {
+            if(grid[i][j] ==1)
+            {
+                result = result + grid[i][j]*4;       
+                if(j!=0 && (grid[i][j-1]==1))
+                    result -=1;
+                if(j!=(gridColSize-1) && (grid[i][j+1]==1))   
+                    result -=1;
+                if(i!=0 && (grid[i-1][j]==1))
+                    result -=1;
+                if(i!=(gridRowSize-1) && (grid[i][j+1]==1))
+                    result -=1;            
+            }
+        }
+    }
+        return result;
+    
+}
