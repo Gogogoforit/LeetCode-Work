4a0bd66d29f27aabdf30410020243742d47389ad
garrulous abyss <2945138243@qq.com>
2017-03-08 14:23:36 +0800

    Create 292. Nim Game.c

---
+++ 292. Nim Game.c
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/nim-game/?tab=Description
+//这题笑死我了2333333333333
+//重要的就是一个化归思想。
+bool canWinNim(int n) {
+    if(n%4==0)
+        return false;
+    else 
+        return true;
+}
