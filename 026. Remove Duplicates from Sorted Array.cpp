e9b4dbaf6b1a4f87a425dc2dc50c020976f391b8
garrulous abyss <2945138243@qq.com>
2018-09-21 15:27:41 +0200

    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ 026. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,13 @@
+//题目链接： 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int len = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[len++] = nums[i];
+            }
+        }
+        return len;
+    }
+};
